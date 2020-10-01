f4345821296df6f1d820a1a7049d7dba775f050d
sandcha <sandcha@users.noreply.github.com>
2020-10-01 11:03:16 +0200

    Copy standard matomo tracking code in home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,22 @@ class HomePage extends PureComponent {
       <Fragment>
         <Head>
           <title>LexImpact</title>
+
+          <!-- Matomo -->
+          <script type="text/javascript">
+            var _paq = window._paq = window._paq || [];
+            /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+            _paq.push(['trackPageView']);
+            _paq.push(['enableLinkTracking']);
+            (function() {
+              var u="https://leximpact.matomo.cloud/";
+              _paq.push(['setTrackerUrl', u+'matomo.php']);
+              _paq.push(['setSiteId', '1']);
+              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+              g.type='text/javascript'; g.async=true; g.src='//cdn.matomo.cloud/leximpact.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+            })();
+          </script>
+          <!-- End Matomo Code -->
         </Head>
         <AppHeader showHomeButton={false} showLoginButton={false} />
         <HomeContent />
