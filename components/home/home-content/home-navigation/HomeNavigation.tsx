dc4fc3d55a9eff0ebc4cfe38c709fc4d4a52458f
sandcha <sandcha@users.noreply.github.com>
2020-11-10 18:44:42 +0100

    Try default yellow color on recrutement CTA

--- components/home/home-content/home-navigation/HomeNavigation.tsx
+++ components/home/home-content/home-navigation/HomeNavigation.tsx
@@ -4,37 +4,39 @@ import { Icon } from "@iconify/react";
 import Router from "next/router";
 import { PureComponent } from "react";
 
+import recrutementStyles from "../../../../pages/recrutement.module.scss";
 import styles from "./HomeNavigation.module.scss";
 
 export class HomeNavigation extends PureComponent {
   render() {
     return (
-      <div className={styles.navigation}>
-        <h1>
-          Estimer les impacts
-          <br />
-          d&apos;une modification de la loi :
-        </h1>
-        <button type="button" onClick={() => Router.push("/ir")}>
-          <img alt="" src="/icons/picto-impot-sur-le-revenu.png" />
-          sur l&apos;impôt sur le revenu
-        </button>
-        <button type="button" onClick={() => Router.push("/dotations")}>
-          <img alt="" src="/icons/picto-dotations-communes.png" />
-          sur les dotations aux communes
-        </button>
-        <div>
-          <a href="https://doodle.com/mm/equipeleximpact/permanence">
-            Prendre un rendez-vous avec LexImpact
-          </a>
+      <div>
+        <div className={styles.navigation}>
+          <h1>
+            Estimer les impacts
+            <br />
+            d&apos;une modification de la loi :
+          </h1>
+          <button type="button" onClick={() => Router.push("/ir")}>
+            <img alt="" src="/icons/picto-impot-sur-le-revenu.png" />
+            sur l&apos;impôt sur le revenu
+          </button>
+          <button type="button" onClick={() => Router.push("/dotations")}>
+            <img alt="" src="/icons/picto-dotations-communes.png" />
+            sur les dotations aux communes
+          </button>
+          <div>
+            <a href="https://doodle.com/mm/equipeleximpact/permanence">
+              Prendre un rendez-vous avec LexImpact
+            </a>
+          </div>
         </div>
-        <div>
+        <div className={recrutementStyles.divRecrutement}>
           <Icon height="30" icon={womanCurlyHaired} width="30" />
           &nbsp;
           <a onClick={() => Router.push("/recrutement")}>
-            Rejoindre l&apos;équipe LexImpact
+            LexImpact recrute. Rejoignez-nous !
           </a>
-          &nbsp;
           <Icon height="30" icon={manCurlyHaired} width="30" />
         </div>
       </div>
