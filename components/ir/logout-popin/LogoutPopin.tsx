6490576a50b9576ed7a13bae836d129a284f8806
Loïc Poullain <loic.poullain@centraliens.net>
2020-11-23 13:45:25 +0100

    Remove withStyles from LogoutPopin

--- components/ir/logout-popin/LogoutPopin.tsx
+++ components/ir/logout-popin/LogoutPopin.tsx
@@ -3,52 +3,34 @@ import PartyPopperIcon from "@iconify/icons-twemoji/party-popper";
 import PeopleHoldingHandsIcon from "@iconify/icons-twemoji/people-holding-hands";
 import { Icon } from "@iconify/react";
 import IconButton from "@material-ui/core/IconButton";
-import { withStyles } from "@material-ui/core/styles";
 import CloseIcon from "@material-ui/icons/Close";
-import PropTypes from "prop-types";
 import { PureComponent } from "react";
 
-const styles = {
-  closeButton: {
-    position: "absolute",
-    right: 10,
-    top: 10,
-  },
-  h1Style: {
-    color: "#000000",
-    fontFamily: "Lato",
-    fontSize: "24px",
-    fontWeight: "bold",
-    marginBottom: "20px",
-    textAlign: "center",
-  },
-  pStyle: {
-    color: "#000000",
-    fontFamily: "Lato",
-    fontSize: "18px",
-    marginBottom: "40px",
-    marginLeft: "40px",
-  },
-};
+import styles from './LogoutPopin.module.scss';
 
-class LogoutPopinComponent extends PureComponent {
+interface Props {
+  fetchMetadataCasTypesHandler: () => void;
+  onClosePopin: () => void;
+}
+
+class LogoutPopin extends PureComponent<Props> {
   componentDidMount() {
     const { fetchMetadataCasTypesHandler } = this.props;
     fetchMetadataCasTypesHandler();
   }
 
   render() {
-    const { classes, onClosePopin } = this.props;
+    const { onClosePopin } = this.props;
     return (
       <div>
-        <IconButton className={classes.closeButton} onClick={onClosePopin}>
+        <IconButton className={styles.closeButton} onClick={onClosePopin}>
           <CloseIcon fontSize="small" />
         </IconButton>
-        <h1 className={classes.h1Style}>
+        <h1 className={styles.title}>
           <Icon inline icon={PartyPopperIcon} width="40" />
           Vous êtes déconnecté de LexImpact IR&nbsp;
         </h1>
-        <p className={classes.pStyle}>
+        <p className={styles.content}>
           Vous devez vous reconnecter afin de pouvoir à nouveau
           <b>&nbsp;simuler les impacts macros&nbsp;</b>
           d’une réforme
@@ -64,10 +46,4 @@ class LogoutPopinComponent extends PureComponent {
   }
 }
 
-LogoutPopinComponent.propTypes = {
-  classes: PropTypes.shape().isRequired,
-  fetchMetadataCasTypesHandler: PropTypes.func.isRequired,
-  onClosePopin: PropTypes.func.isRequired,
-};
-
-export default withStyles(styles)(LogoutPopinComponent);
+export default LogoutPopin;
