9f3275838a85f96f566526d60e9a6d39836ef33f
Loïc Poullain <loic.poullain@centraliens.net>
2020-11-24 10:50:16 +0100

    Remove unused components

--- components/ir/ajouter-cas-types/form/mui-extras-components/index.js
+++ components/ir/ajouter-cas-types/form/mui-extras-components/index.js
@@ -1,3 +1,1 @@
-export { default as MUIDotMenu } from "./materialui-dot-menu";
 export { default as MUINumberStepper } from "./materialui-number-stepper";
-export { default as MUIDropdownMenu } from "./materialui-dropdown-menu";
