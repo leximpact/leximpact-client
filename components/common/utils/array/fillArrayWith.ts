23a4d437052909a857d74a81589c802649ad1306
Loïc Poullain <loic.poullain@centraliens.net>
2020-11-30 10:50:58 +0100

    Move last utils from JS to TS

--- components/common/utils/array/fillArrayWith.ts
+++ components/common/utils/array/fillArrayWith.ts
@@ -1,5 +1,5 @@
-const fillArrayWith = (length, iteratorFunc) => Array(length)
-  .fill()
+const fillArrayWith = (length: number, iteratorFunc?) => Array(length)
+  .fill(undefined)
   .map((value, index) => (iteratorFunc && iteratorFunc(index)) || index);
 
 export default fillArrayWith;
