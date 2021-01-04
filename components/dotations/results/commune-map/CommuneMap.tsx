97df33b0cdb503f38b6a5c46a6573c88df819ad1
sandcha <sandcha@users.noreply.github.com>
2021-01-04 09:54:12 +0100

    Init CommuneMap component files

---
+++ components/dotations/results/commune-map/CommuneMap.tsx
@@ -0,0 +1,12 @@
+import { PureComponent } from "react";
+
+import { Card } from "../../../common";
+import styles from "./CommuneMap.module.scss";
+
+export class CommuneMap extends PureComponent {
+    render() {
+      return (
+        //<Card content1={}/>
+      );
+    }
+  }
---
+++ components/dotations/results/commune-map/index.ts
@@ -0,0 +1,1 @@
+export { CommuneMap } from "./CommuneMap";
