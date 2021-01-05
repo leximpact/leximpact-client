9edc5fb354b6f2dada22d22bfad9926c620bba53
sandcha <sandcha@users.noreply.github.com>
2021-01-05 22:34:10 +0100

    Init CommuneMap content

--- components/dotations/results/commune-map/CommuneMap.tsx
+++ components/dotations/results/commune-map/CommuneMap.tsx
@@ -1,12 +1,19 @@
 import { PureComponent } from "react";
 
 import { Card } from "../../../common";
+import "./build-map";
 import styles from "./CommuneMap.module.scss";
 
 export class CommuneMap extends PureComponent {
-    render() {
-      return (
-        //<Card content1={}/>
-      );
-    }
+  render() {
+    return (
+      <Card
+        content1={(
+          <div
+            id="mapid"
+          />
+        )}
+        title="Proportion des communes nouvellement éligibles et non-éligibles" />
+    );
   }
+}
