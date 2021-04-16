e46176ace292feccec665ba6505f3cd15a01265c
benoit-cty <6603048+benoit-cty@users.noreply.github.com>
2021-04-16 18:16:58 +0200

    Migration du tracking Matomo

--- components/common/tracker/Matomo.tsx
+++ components/common/tracker/Matomo.tsx
@@ -11,11 +11,11 @@ export class Matomo extends PureComponent {
           _paq.push(['trackPageView']);
           _paq.push(['enableLinkTracking']);
           (function() {
-            var u="https://leximpact.matomo.cloud/";
+            var u="https://matomo.leximpact.dev/";
             _paq.push(['setTrackerUrl', u+'matomo.php']);
             _paq.push(['setSiteId', '1']);
             var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-            g.type='text/javascript'; g.async=true; g.src='//cdn.matomo.cloud/leximpact.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+            g.type='text/javascript'; g.async=true; g.src='//matomo.leximpact.dev/matomo.js'; s.parentNode.insertBefore(g,s);
           })();`,
         }}
         type="text/javascript" />
